63adaad4c90beb843a7f2957af51d56a7691b8c1
n0stalg1a <bradleycollins82@gmail.com>
2024-07-07 15:39:25 +1000

    5: List Overlap

---
+++ exercise05.py
@@ -0,0 +1,44 @@
+# 5: List Overlap
+# Write a program that returns a list that contains only the elements 
+# that are common between the lists (without duplicates). Make sure your 
+# program works on two lists of different sizes.
+
+# Extras:
+
+# Randomly generate two lists to test this
+# Write this in one line of Python (don’t worry if you can’t figure this 
+# out at this point - we’ll get to it soon)
+
+import random
+
+a = []
+b = []
+
+# Extras 1
+def createList(newlist):
+    listlength = random.randint(10, 20)
+    for i in range(1, listlength):
+        num = random.randint(1, 25)
+        if num not in newlist:
+            newlist.append(num)
+
+createList(a)
+createList(b)
+a.sort()
+b.sort()
+print(a)
+print(b)
+
+
+c = []
+
+for i in a:
+    for j in b:
+        if j == i and j not in c:
+            c.append(j)
+
+
+print(c)
+
+# Extras 2
+print(list(set(i for i in a if i in b)))
