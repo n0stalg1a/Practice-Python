a403d864a08c5e02f4d1d22564ef717b2e5811be
n0stalg1a <bradleycollins82@gmail.com>
2024-07-07 16:28:58 +1000

    12: List Ends

---
+++ exercise12.py
@@ -0,0 +1,12 @@
+# 12: List Ends
+# Write a program that takes a list of numbers for example, 
+a = [5, 10, 15, 20, 25]
+# and makes a new list of only the first and last elements of the given list. 
+# For practice, write this code inside a function.
+
+
+def firstandlast(list):
+    b = [list[0], list[-1]]
+    print(b)
+
+firstandlast(a)
