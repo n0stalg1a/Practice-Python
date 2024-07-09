8f93e411010e120e279b70b6645a493bcdd5bdd7
n0stalg1a <bradleycollins82@gmail.com>
2024-07-09 20:50:39 +1000

    7: List Comprehensions

--- exercise07.py
+++ exercise07.py
@@ -1,7 +1,7 @@
 # 7: List Comprehensions
-# Let’s say I give you a list saved in a variable: 
+# Let’s say I give you a list saved in a variable:
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-# Write one line of Python that takes this list a and makes a new list that has 
+# Write one line of Python that takes this list a and makes a new list that has
 # only the even elements of this list in it.
 
 print([i for i in a if i % 2 == 0])
