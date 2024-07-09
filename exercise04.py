fe36d02c659fd0961c68fe0e13ea94a70296a9e3
n0stalg1a <bradleycollins82@gmail.com>
2024-07-09 20:47:40 +1000

    4: Divisors

--- exercise04.py
+++ exercise04.py
@@ -1,8 +1,8 @@
 # 4: Divisors
-# Create a program that asks the user for a number and then prints 
-# out a list of all the divisors of that number. (If you don’t know 
-# what a divisor is, it is a number that divides evenly into another 
-# number. For example, 13 is a divisor of 26 because 26 / 13 has 
+# Create a program that asks the user for a number and then prints
+# out a list of all the divisors of that number. (If you don’t know
+# what a divisor is, it is a number that divides evenly into another
+# number. For example, 13 is a divisor of 26 because 26 / 13 has
 # no remainder.)
 
 usernum = int(input('Please enter a number: '))
