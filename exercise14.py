2d156b533af6b7fa6c40301acde93c040af5c1d7
n0stalg1a <bradleycollins82@gmail.com>
2024-07-09 21:06:38 +1000

    14: List Remove Duplicates

--- exercise14.py
+++ exercise14.py
@@ -1,14 +1,14 @@
 # 14: List Remove Duplicates
-# Write a program (function!) that takes a list and returns a new list that 
+# Write a program (function!) that takes a list and returns a new list that
 # contains all the elements of the first list minus all the duplicates.
 
 # Extras:
-# Write two different functions to do this - one using a loop and constructing 
+# Write two different functions to do this - one using a loop and constructing
 # a list, and another using sets.
-# Go back and do Exercise 5 using sets, and write the solution for that in 
+# Go back and do Exercise 5 using sets, and write the solution for that in
 # a different function.
 
-a = [1,1,1,2,2,2,3,3,3]
+a = [1, 1, 1, 2, 2, 2, 3, 3, 3]
 
 
 def removeduplicatesloop(list):
