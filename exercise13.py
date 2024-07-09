e886e43ee6370a9570c5d106da44afd705363f48
n0stalg1a <bradleycollins82@gmail.com>
2024-07-09 21:06:23 +1000

    13: Fibonacci

--- exercise13.py
+++ exercise13.py
@@ -1,15 +1,16 @@
 # 13: Fibonacci
-# Write a program that asks the user how many Fibonnaci numbers 
-# to generate and then generates them. Take this opportunity to 
-# think about how you can use functions. Make sure to ask the user 
+# Write a program that asks the user how many Fibonnaci numbers
+# to generate and then generates them. Take this opportunity to
+# think about how you can use functions. Make sure to ask the user
 # to enter the number of numbers in the sequence to generate.
-# (Hint: The Fibonnaci seqence is a sequence of numbers where the 
-# next number in the sequence is the sum of the previous two numbers 
+# (Hint: The Fibonnaci seqence is a sequence of numbers where the
+# next number in the sequence is the sum of the previous two numbers
 # in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
 
 
 def startsequence():
-    a = int(input('Please enter a number of Fibonacci sequence numbers to generate: '))
+    a = int(input('Please enter a number of Fibonacci sequence numbers to'
+                  ' generate: '))
     if a <= 0:
         b = []
         print(b)
@@ -24,5 +25,6 @@ def startsequence():
         for i in range(2, a):
             b.append(b[i - 1] + b[i - 2])
         print(b)
+
 
 startsequence()
