862004245f8d69851f376de84e6c0ab7f7e3a56b
n0stalg1a <bradleycollins82@gmail.com>
2024-07-09 21:03:46 +1000

    9: Guessing Game One

--- exercise09.py
+++ exercise09.py
@@ -1,12 +1,12 @@
 # 9: Guessing Game One
-# Generate a random number between 1 and 9 (including 1 and 9). 
-# Ask the user to guess the number, then tell them whether they guessed too low, 
-# too high, or exactly right. 
+# Generate a random number between 1 and 9 (including 1 and 9).
+# Ask the user to guess the number, then tell them whether they guessed too
+# low, too high, or exactly right.
 # (Hint: remember to use the user input lessons from the very first exercise)
 
 # Extras:
 # Keep the game going until the user types “exit”
-# Keep track of how many guesses the user has taken, and when the game ends, 
+# Keep track of how many guesses the user has taken, and when the game ends,
 # print this out.
 
 import random
@@ -39,5 +39,6 @@ def game():
         else:
             guesses += 1
             print('Too high.')
+
 
 game()
